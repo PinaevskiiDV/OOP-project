21145d1e54a5e567f8b580e4fc5326284e0aca17
Dmitrii <pinochet37@mail.ru>
2023-02-13 14:47:09 +0300

    Create new rep

---
+++ src/main/java/Seminar001/Program/Beer.java
@@ -0,0 +1,22 @@
+package OOP.Seminar001.Program;
+
+public class Beer extends Product {
+
+    private Integer alcohol = 0;
+
+    public Beer(String name, Double price) {
+        super(name, price);
+    }
+
+    public Beer(String name, Double price, Integer alcohol) {
+        this(name, price);
+        this.alcohol = alcohol;
+
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s alcohol = %d  " , super.toString(), alcohol);
+    }
+
+}
